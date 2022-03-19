b4adb8ae13281c94569e482a419f2f67a4e6c9a3
Tiago Oliveira <tiago.o.tocha@gmail.com>
2022-03-19 03:14:58 +0000

    generateConfigFile.py

--- generateConfigFile.py
+++ generateConfigFile.py
@@ -1,67 +1,36 @@
 import random as r
 import string as s
+import time as t
 
-passwordOpt = s.ascii_uppercase + s.ascii_lowercase + s.digits
+from faker import Faker
+from unidecode import unidecode
 
-array = ["João", "Luís", "José", "Alexandre", "Tiago", "Miguel", "Pedro", "Tomas",
-         "Rodrigo", "Fransico", "Guillherme", "Santiago", "Renato", "Alberto", "Carlos"]
-arrayf = ["Inês", "Joana", "Tatiana", "Rita", "Ana", "Maria", "Mariana",
-          "Cristina", "Teresa", "Francisca", "Carolina", "Mafalda", "Camila"]
-fac_dep = ["FCTUC - DEEC", "FCTUC - DEI", "FCTUC - DEQ", "FCTUC - DARQ",
-           "FCTUC - DEM", "FCTUC - DF", "FCTUC - DQ", "FCTUC - DM"]
-texto = []
-for i in range(10):
-    password = ""
-    CCNumber = ""
-    phoneNumber = "9"
+if __name__ == "__main__":
+    fake = Faker("pt_PT")
 
-    for i in range(8):
-        password += r.choice(passwordOpt)
-        CCNumber += r.choice(s.digits)
-        phoneNumber += r.choice(s.digits)
-    nc = ""
-    posa = 0
-    sexo = r.randint(0, 1)
-    if (sexo == 1):
-        x = r.randint(1, 2)
-        for l in range(x):
-            pos = r.randint(0, 12)
-            if(pos == posa):
-                pos = pos+1
-            posa = pos
-            nc = nc+array[pos]+" "
-        pos1 = r.randint(0, 12)
-        pos2 = r.randint(0, 12)
-        pos3 = r.randint(0, 5)
-        if(pos2 == pos1):
-            pos2 = pos2-1
+    passwordOpt = s.ascii_uppercase + s.ascii_lowercase + s.digits
 
-        if nc.lower() not in texto:
-            nc = nc.lower().replace(" ", "") + "\t" + password + "\t" + fac_dep[int(i/6)-1] + "\t" + CCNumber + "\t" + phoneNumber + "\t"+str(
-                r.randint(0, 30))+"/"+str(r.randint(1, 13))+"/" + str(r.randint(2022, 2027)) + "\n"
-            texto.append(nc)
+    fac_dep = ["FCTUC - DEEC", "FCTUC - DEI", "FCTUC - DEQ", "FCTUC - DARQ",
+               "FCTUC - DEM", "FCTUC - DF", "FCTUC - DQ", "FCTUC - DM"]
 
-    else:
-        x = r.randint(1, 2)
-        for l in range(x):
-            pos = r.randint(0, 12)
-            if (pos == posa):
-                pos = pos + 1
-            posa = pos
-            nc = nc + arrayf[pos] + " "
-        pos1 = r.randint(0, 12)
-        pos2 = r.randint(0, 12)
-        pos3 = r.randint(0, 5)
-        if (pos2 == pos1):
-            pos2 = pos2 - 1
+    with open("C:/Users/Tiago Oliveira/OneDrive - Universidade de Coimbra/3º Ano/SD/ucDrive/src/userdata.txt",
+              "w") as f:
+        for _ in range(10):
+            password = ""
+            CCNumber = ""
+            phoneNumber = "9" + r.choice("1236")
+            username = fake.user_name()
+            address = unidecode(fake.freguesia())
+            department = r.choice(fac_dep)
+            curYear = int(t.strftime("%Y", t.localtime()))
+            expDate = str(r.randint(1, 31)) + "/" + str(r.randint(1, 12)) + "/" + str(r.randint(curYear, curYear + 5))
 
-        if nc.lower() not in texto:
-            nc = nc.lower().replace(" ", "") + "\t" + password + "\t" + fac_dep[int(i/6)-1] + "\t" + CCNumber + "\t" + phoneNumber + "\t"+str(
-                r.randint(0, 30))+"/"+str(r.randint(1, 13))+"/" + str(r.randint(2022, 2027)) + "\n"
-            texto.append(nc)
+            for i in range(8):
+                password += r.choice(passwordOpt)
+                CCNumber += r.choice(s.digits)
+                if i < 7:
+                    phoneNumber += r.choice(s.digits)
 
-
-f = open("C:/Users/Tiago Oliveira/OneDrive - Universidade de Coimbra/3º Ano/SD/ucDrive/src/userdata.txt", "w")
-for j in texto:
-    f.write(j)
-f.close()
+            info = username + "\t" + password + "\t" + address + "\t" + department + "\t" + phoneNumber + "\t" + CCNumber + "\t" + expDate + "\n"
+            print(info, end="")
+            f.write(info)
