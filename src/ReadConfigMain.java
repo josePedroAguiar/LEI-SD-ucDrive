f7129505056e48f1e7d5631bbd79edc9fc9ad2af
José Pedro Silva Aguiar <joseaguiar@student.dei.uc.pt>
2022-03-23 02:46:56 +0000

    Login quase na totalidade implementado

--- src/ReadConfigMain.java
+++
@@ -1,8 +0,0 @@
- 
-import java.io.IOException;
-public class ReadConfigMain {
-    public static void main(String[] args) throws IOException {
-		PropertyValues properties = new PropertyValues();
-		properties.getPropValues();
-	}
-}
