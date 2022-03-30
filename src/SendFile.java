3f6b9ad206bcfd46a397f76d493865bd17c0b90e
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2022-03-30 14:56:32 +0100

    ai ai

--- src/SendFile.java
+++ src/SendFile.java
@@ -20,7 +20,7 @@ public class SendFile implements Runnable{
 
 
         try {
-            String name="filegeneretor.py";
+            String name="ReceiveFile.java";
             Path path = Paths.get(name);
             int length=(int)Files.size(path);
             BufferedInputStream bis;
@@ -37,8 +37,43 @@ public class SendFile implements Runnable{
             ds.send(dp);
             ds.close();
 
+            int nPackage=(int)(length/1024);
+            int lastPackageSize=(int)(length%1024);
 
+            bis = new BufferedInputStream(new FileInputStream(name));
+            ds = new DatagramSocket();
+           
+            byte [] allPacket = new byte[length];
+            bis.read(allPacket);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            int i;
+            for(i=0;i<nPackage;i++){
+                outputStream = new ByteArrayOutputStream();
+                outputStream.write(allPacket,1024*i,1024);
+                buf = new byte[1024];
+                buf= outputStream.toByteArray( );
+                bis.read(buf);
+                dp = new DatagramPacket(buf,buf.length, InetAddress.getByName("127.0.0.1"),10001);
+                ds.send(dp);
+                System.out.println("T_T");
+            }
+            System.out.println("-_-");
+            System.out.println(i);
+            if(lastPackageSize!=0){
+                outputStream = new ByteArrayOutputStream();
+                outputStream.write(allPacket,1024*i,lastPackageSize);
+                buf = new byte[lastPackageSize];
+                buf= outputStream.toByteArray( );
+                bis.read(buf);
+                dp = new DatagramPacket(buf,buf.length, InetAddress.getByName("127.0.0.1"),10001);
+                ds.send(dp);
+             
+             
+            }
+            ds.close();
+            bis.close();
 
+            /*
             bis = new BufferedInputStream(new FileInputStream(name));
             ds = new DatagramSocket();
             buf = new byte[length];
@@ -47,11 +82,15 @@ public class SendFile implements Runnable{
             ds.send(dp);
             ds.close();
             bis.close();
+            */
+
         } catch (IOException e) {
 
 
             e.printStackTrace();
         }
+
+          
     }
     public static void main(String[] args) {
 
